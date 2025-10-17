ad2df4cf021e891e3c97f7054fd2ed97d6a5ce89
Alberto Lima <alberto.btlima@outlook.com>
2025-10-17 12:32:35 -0300

    add: axios

--- 05-react-consumindo-APIs-HTTP-no-projeto/code-connect2.0/src/components/CardPost/index.jsx
+++ 05-react-consumindo-APIs-HTTP-no-projeto/code-connect2.0/src/components/CardPost/index.jsx
@@ -5,6 +5,7 @@ import { ThumbsUpButton } from "./ThumbsUpButton";
 import { ModalComment } from "../ModalComment";
 import { Link } from "react-router";
 import { useState } from "react";
+import { http } from "../../api";
 
 export const CardPost = ({ post }) => {
   const [likes, setLikes] = useState(post.likes);
@@ -12,17 +13,20 @@ export const CardPost = ({ post }) => {
   const handleLikeButton = () => {
     const token = localStorage.getItem("access_token");
 
-    fetch(`http://localhost:3000/blog-posts/${post.id}/like`, {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    }).then((response) => {
-      if (response.ok) {
+    http
+      .post(
+        `blog-posts/${post.id}/like`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then(() => {
         setLikes((oldState) => oldState + 1);
         console.log("Like");
-      }
-    });
+      });
   };
 
   return (
