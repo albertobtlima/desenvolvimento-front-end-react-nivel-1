79fa8bee1a303f189e4a46175aba1ba5fb0e4771
Alberto Lima <alberto.btlima@outlook.com>
2025-10-22 10:42:50 -0300

    extraido logica de get do feed

--- 07-react-utilizando-graphQL-para-integracao-de-dados/react-runner-circle/src/main.jsx
+++ 07-react-utilizando-graphQL-para-integracao-de-dados/react-runner-circle/src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
 
 const client = new ApolloClient({
   uri: "http://localhost:3001/graphql",
